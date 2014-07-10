e91bed1c09b9c725d8e0df316bcd3f7ef3c977e6
unboundedcoin <unboundedcoin@users.noreply.github.com>
2014-07-10 03:19:01 +0200

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -48,8 +48,8 @@
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT              200    //by default, blocks count in blocks downloading
 #define CRYPTONOTE_PROTOCOL_HOP_RELAX_COUNT             3      //value of hop, after which we use only announce of new block
 
-#define P2P_DEFAULT_PORT                                7880
-#define RPC_DEFAULT_PORT                                7881
+#define P2P_DEFAULT_PORT                                12754
+#define RPC_DEFAULT_PORT                                12755
 #define COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT           1000
 
 #define P2P_LOCAL_WHITE_PEERLIST_LIMIT                  1000
